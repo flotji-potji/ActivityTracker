603eda0154d504dc9f869ad98318c5aedc9f5536
Florian Schmidt <nme199911@gmail.com>
2021-07-13 20:19:04 +0200

    further bug fixes

--- delete_activity_tracker.py
+++ delete_activity_tracker.py
@@ -5,8 +5,8 @@ import shutil
 def main():
     os.system('sudo systemctl stop activity_tracker.service')
     os.system('sudo systemctl disable activity_tracker.service')
-    os.remove('/etc/systemd/system/activity_tracker.service')
-    os.remove('/usr/lib/systemd/system/activity_tracker.service')
+    os.remove('/etc/systemd/system/activity_tracker.*')
+    os.remove('/usr/lib/systemd/system/activity_tracker.*')
     os.system('sudo systemctl daemon-reload')
     os.system('sudo systemctl reset-failed')
     shutil.rmtree('ActivityTracker')
