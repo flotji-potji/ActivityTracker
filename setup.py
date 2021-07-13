9e38c8713cac2651a3a5f02ea424d82b586950b5
Florian Schmidt <nme199911@gmail.com>
2021-07-13 21:26:54 +0200

    okay this seems fine

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ import shutil
 def main():
     shutil.move('/home/pi/ActivityTracker/activity_tracker.service', '/etc/systemd/system/activity_tracker.service')
     shutil.move('/home/pi/ActivityTracker/delete_activity_tracker.py', '/home/pi/delete_activity_tracker.py')
+    shutil.move('/home/pi/ActivityTracker/gyro.py', '/usr/local/bin/gyro.py')
+    os.system('sudo chmod 744 /usr/local/bin/gyro.py')
+    os.system('sudo chmod 644 /etc/systemd/system/activity_tracker.service')
     os.system('sudo systemctl enable activity_tracker.service')
     os.system('sudo systemctl start activity_tracker.service')
 
